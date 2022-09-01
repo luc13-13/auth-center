83888a8c7a4d4fdf3d38601f3248763b2d66ac11
luc13-13 <lucheng@mail.bnu.edu.cn>
2022-09-01 11:54:56 +0800

    开启服务发现和feign

--- src/main/java/com/lc/auth/center/Bootstrap.java
+++ src/main/java/com/lc/auth/center/Bootstrap.java
@@ -3,13 +3,14 @@ package com.lc.auth.center;
 import com.lc.auth.center.constant.JwtProperties;
 import com.lc.auth.center.constant.LucSysProperties;
 import com.lc.auth.center.constant.ShiroProperties;
-
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author: lucheng
@@ -17,9 +18,11 @@ import org.springframework.cache.annotation.EnableCaching;
  * @version: 1.0
  */
 @SpringBootApplication
-@MapperScan("mapper/*.xml")
 @EnableCaching
+@MapperScan("mapper/*.xml")
 @EnableConfigurationProperties({LucSysProperties.class, JwtProperties.class, ShiroProperties.class, RedisProperties.class})
+@EnableDiscoveryClient
+@EnableFeignClients
 public class Bootstrap {
     public static void main(String[] args) {
         SpringApplication.run(Bootstrap.class, args);
